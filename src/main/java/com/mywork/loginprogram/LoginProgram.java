e6bc74790162cde513013df2029e8969f4c13403
USER <USER@192.168.0.28>
2024-02-05 10:58:15 +0300

    Updated Commit

--- src/main/java/com/mywork/loginprogram/LoginProgram.java
+++ src/main/java/com/mywork/loginprogram/LoginProgram.java
@@ -18,9 +18,13 @@ public class LoginProgram
         
         String correctUsername = "user";
         String correctPassword = "password";
+        
+//        This code sets the correct username and password that the user needs to provide for a successful login.
+        
         int maxAttempts = 3;
         int attempts = maxAttempts;
         
+//        This code sets the maximum number of login attempts (maxAttempts) and initializes the variable attempts to keep track of the remaining login attempts.
         
         while (attempts > 0)
         {System.out.print("Enter your username: ");
@@ -29,11 +33,14 @@ public class LoginProgram
         System.out.print("Enter your password: ");
         String inputPassword = loginScanner.nextLine();
         
-       
+//       This code prompts the user to enter their username and password and reads the input using the Scanner object.
+        
         if (inputUsername.equals(correctUsername) && inputPassword.equals(correctPassword))
         { System.out.println("Login successful. Welcome!");
           break;
         }
+        
+//        This code checks if the entered username and password match the correct credentials. If so, prints a success message, and the loop is exited with the break statement.
         
         else
         {System.out.println("Login failed. Please try again.");
@@ -47,9 +54,11 @@ public class LoginProgram
         {System.out.println("Login attempts exceeded. Exiting program.");
         }
 
+//        This code checks if the maximum login attempts have been reached. If so, prints a message indicating that login attempts are exceeded.
         
         loginScanner.close();
         
+//        This code closes the Scanner.
         
         }
     }
